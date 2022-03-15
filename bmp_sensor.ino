01ad7dcf3d6c1686e77eb0a16b831bc7aec9c055
Viraj Mehta <54108753+viraj-coder@users.noreply.github.com>
2022-03-15 20:37:50 +0530

    Add files via upload

---
+++ bmp_sensor.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+  Serial.println("Could not found BMP180");
+  while (1) {}
+  }
+}
+  
+void loop() {
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+    
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+ 
+  Serial.println();
+  delay(500);
+}
